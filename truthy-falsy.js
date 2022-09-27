62422018863343603868c5545a02bff5146556e3
LeninMatos <leninmatos@gmail.com>
2022-09-27 14:17:46 -0300

    criação de novo arquivo

---
+++ truthy-falsy.js
@@ -0,0 +1,30 @@
+// boolean 
+
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true 
+
+//console.log(0 == false)
+//console.log(1 == true)
+//console.log(" " == false)
+
+
+
+// null - objeto
+// undefined - let sem valor, é primeiramente "não definida"
+
+let minhaVar;
+let varNull = null;
+
+console.log (minhaVar)
+console.log (varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log (typeof numero)
+console.log (typeof texto)
