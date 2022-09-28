c3806d250be9545c8c4d136f61c7f5f3f0e37fa7
LeninMatos <leninmatos@gmail.com>
2022-09-28 10:26:49 -0300

    Novo arquivo de comparações

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == (comparção implícita) compara só o valor.
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// === (comparção explícita) compara o valor e o tipo de dado. --> As boas práticas pedem para usar essa comparação
+
+console.log(numero === texto)
+
+// usar 
+Number()
+String()
+
+//typeoff 
+
+console.log(typeof numero)
+console.log(typeof texto)
+
