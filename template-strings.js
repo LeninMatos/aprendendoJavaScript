e79e6333cd794fdb4174ac904db710e2e0fdda53
LeninMatos <leninmatos@gmail.com>
2022-09-28 13:38:53 -0300

    Adição de novo arquivo

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Lênin";
+const idade = 2021-1986;
+const cidadeDeNascimento = "Campo Grande";
+
+//const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " anos" +" e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e eu nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
