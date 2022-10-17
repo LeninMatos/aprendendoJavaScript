22834adfcfbb2805af84f6e512c165201fcc9d52
LeninMatos <leninmatos@gmail.com>
2022-10-17 15:51:38 -0300

    Novos Arqivos

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+function apresentar(nome) {
+    return `meu nome é ${nome}`;
+}
+
+//Arrow funciton
+let apresentarArrow;
+
+apresentarArrow = nome => `meu nome é ${nome}`;
+
+console.log (apresentar(Lênin));
+console.log(apresentarArrow(João));
---
+++ expressoes-funcao.js
@@ -0,0 +1,22 @@
+// Declaração de função
+function minhaFuncao(param) {
+    //bloco de código
+}
+
+minhaFuncao("param")
+
+// Expressão de função
+
+//console.log(soma (1 ,1))
+const  soma = function(num1, num2) {return num1 + num2}
+
+
+
+
+// DIFERENÇA PRINCIPAL
+// funções e var são "listadas" no topo
+console.log (apresentar)
+
+function apresentar( ) {
+    return "Olá"
+}
